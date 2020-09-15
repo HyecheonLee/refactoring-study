a3db4b72006495c127f85175a7a4db150051b608
hyecheon <rainbow880616@gmail.com>
2020-09-15 22:24:17 +0900

    6.2 - 3-2 인라인.

--- src/ch06/InlineFunction02.js
+++ src/ch06/InlineFunction02.js
@@ -1,14 +1,6 @@
-function rating(aDriver) {
-  return aDriver.numberOfLateDeliveries > 5 ? 2 : 1;
-}
-
 function reportLines(aCustomer) {
   const lines = [];
-  gatherCustomerData(lines, aCustomer);
+  lines.push(['name', aCustomer.name]);
+  lines.push(['location', aCustomer.location]);
   return lines
-}
-
-function gatherCustomerData(out, aCustomer) {
-  out.push(['name', aCustomer.name]);
-  out.push(['location', aCustomer.location]);
 }
