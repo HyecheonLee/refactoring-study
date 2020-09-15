639ab1124c660784c47331d6c46831b44fb83de4
hyecheon <rainbow880616@gmail.com>
2020-09-15 21:49:32 +0900

    6.1 - 2

--- src/ch06/ExtractFunction.js
+++ src/ch06/ExtractFunction.js
@@ -1,8 +1,12 @@
 function printOwing(invoice) {
   printBanner();
   const outstanding = calculateOutstanding();
-  
+
   //세부 사항 출력
-  console.log(`고객명 ${invoice.customer}`)
-  console.log(`채무액 ${outstanding}`)
+  printDetails(outstanding);
+
+  function printDetails(outstanding) {
+    console.log(`고객명 ${invoice.customer}`)
+    console.log(`채무액 ${outstanding}`)
+  }
 }
