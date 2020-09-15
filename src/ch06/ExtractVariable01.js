cd585ae047a058a26f2bd8c27b57cf0255a02997
hyecheon <rainbow880616@gmail.com>
2020-09-15 22:40:25 +0900

    6.3 - 변추 추출하기.

--- src/ch06/ExtractVariable01.js
+++ src/ch06/ExtractVariable01.js
@@ -1,5 +1,6 @@
 export function price(order) {
-  return order.quantity * order.itemPrice - Math.max(0, order.quality - 500)
-      * order.itemPrice * 0.05 + Math.min(order.quality * order.itemPrice * 0.1,
-          100);
+  const basePrice = order.quantity * order.itemPrice;
+  const quantityDiscount = Math.max(0, order.quality - 500) * order.itemPrice * 0.05;
+  const shipping = Math.min(order.quality * order.itemPrice * 0.1, 100);
+  return basePrice - quantityDiscount + shipping;
 }
