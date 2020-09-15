380b59172abf8d0f8a31986d0f4968bed2410c7f
hyecheon <rainbow880616@gmail.com>
2020-09-15 22:47:37 +0900

    6.3 - 2 변수추출.

--- src/ch06/ExtractVariable02.js
+++ src/ch06/ExtractVariable02.js
@@ -12,8 +12,19 @@ class Order {
   }
 
   get price() {
-    return this.quantity * this.itemPrice - Math.max(0, this.quantity - 500)
-        * this.itemPrice * 0.05 + Math.min(this.quantity * this.itemPrice * 0.1,
-            100)
+    return this.basPrice - this.quantityDiscount + this.shipping
+  }
+
+  get shipping() {
+    return Math.min(this.quantity * this.itemPrice * 0.1, 100);
+  }
+
+  get quantityDiscount() {
+    return Math.max(0, this.quantity - 500)
+        * this.itemPrice * 0.05;
+  }
+
+  get basPrice() {
+    return this.quantity * this.itemPrice;
   }
 }
